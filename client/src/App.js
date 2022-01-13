1e0a3a922b525766c7309ada64e8fc7d6a1bd23f
Mikhail <mikssur@gmail.com>
2022-01-13 19:18:31 +0300

    layout created

---
+++ client/src/App.js
@@ -0,0 +1,61 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Nav from './components/Nav/Nav.jsx'
+import Home from "./components/Home/Home.jsx";
+import Category from "./components/Category/Category.jsx";
+import ServiceList from "./components/ServiceList/ServiceList.jsx"
+import Service from "./components/Service/Service.jsx"
+import About from "./components/About/About.jsx"
+import Review from "./components/Review/Review.jsx"
+import Admin from "./components/Admin/Admin.jsx"
+import Contact from "./components/Contact/Contact.jsx"
+import Portfolio from "./components/Portfolio/Portfolio.jsx"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Nav />
+
+      <Switch>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/category">
+          <Category />
+        </Route>
+
+        <Route exact path="/category/:categoryname">
+          <ServiceList />
+        </Route>
+
+        <Route exact path="/category/:categoryname/:service">
+          <Service />
+        </Route>
+
+        <Route exact path="/about">
+          <About />
+        </Route>
+
+        <Route exact path="/review">
+          <Review />
+        </Route>
+
+        <Route exact path="/admin">
+          <Admin />
+        </Route>
+
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+
+        <Route exact path="/portfolio">
+          <Portfolio />
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/Nav/Nav.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Nav(props) {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/category">Category</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/review">Review</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact Us</Link>
+      </li>
+    </ul>
+  );
+}
+
+export default Nav;
