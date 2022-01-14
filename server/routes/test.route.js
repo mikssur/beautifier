a16e7e7279cafe3d431b96d5307baa10a9ab6ee2
Maxi2695 <Maxi2695@yandex.ru>
2022-01-14 14:48:13 +0300

    update package-lock, routes

--- server/routes/test.route.js
+++ server/routes/test.route.js
@@ -4,6 +4,8 @@ route.get('/categories', (req, res) => {
   res.json(['Маникюр', 'Педикюр']);
 });
 
+route.get('/categories/:categoryname', (req, res) => {
+  res.json('Услуга');
+});
+
 module.exports = route;
-
-
