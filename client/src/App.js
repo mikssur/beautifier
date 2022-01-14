93da53b50a6b0764850e7663c176a8b86f878f3c
Daria <dariasb612@gmail.com>
2022-01-14 11:50:03 +0300

    home 1/2

--- client/src/App.js
+++ client/src/App.js
@@ -54,6 +54,7 @@ function App() {
         </Route>
 
       </Switch>
+
     </BrowserRouter>
   );
 }
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 function Home(props) {
   return (
     <div>
-      
+      <img src="#" />
+      <p>ДЛЯ ТЕХ, КТО ИЗ ЛУЧШЕГО ВЫБИРАЕТ ИСКЛЮЧИТЕЛЬНОЕ!</p>
+      <p>BEAUTIFIER АДРЕС: КУЗНЕЦОВЫЙ ПЕРЕУЛОК 5/2</p>
+      <button>Записаться</button>
+
     </div>
   );
 }
