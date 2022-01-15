0d9c14e53d5e54a260a6f31ef1196a352035b799
Maxi2695 <Maxi2695@yandex.ru>
2022-01-15 16:58:32 +0300

    create reviews

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import { appReducer } from './appReducer';
 // import { categoriesReducer } from './categoriesReducer';
+import reviewsReducer from './reviewsReducer';
 
 
 export const rootReducer = combineReducers({
-  appReducer,
+  appReducer, reviewsReducer
   // categoriesReducer,
 });
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,10 +2,10 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './reducers/rootReducer';
 import createSagaMiddleware from 'redux-saga';
-import {mySaga} from '../redux/sagas/sagas';
+// import {mySaga} from '../redux/sagas/sagas';
 const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware),));
-sagaMiddleware.run(mySaga)
+// sagaMiddleware.run(mySaga)
 
 export default store;
