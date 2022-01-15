00164696235e1dd4732959a89e4a90aa0b8cdd7c
Mikhail <mikssur@gmail.com>
2022-01-15 17:59:15 +0300

    small changes

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,6 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
-
 
 const app = (
   <Provider store={store}>
