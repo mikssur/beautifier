f16d5e1168d2778dcfef0a001733e4a36dfc5ffb
YanaBagrova <yanabag@mail.ru>
2022-01-20 12:33:59 +0300

    reviewedit

--- client/src/components/AdminReview/ReviewCardDelete.jsx
+++ client/src/components/AdminReview/ReviewCardDelete.jsx
@@ -1,30 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Modal from './Modal';
 
 
-function ReviewCard({ name, text, isValid, id }) {
+function ReviewCardDelete({ name, text, isValid, id }) {
   const dispatch = useDispatch()
   const reviewId = id
   const currentURL = window.location.href
   console.log('CURRENT URL V REVIEW CARD', currentURL)
   //const urll = currentURL.split().splice(0, 22)
+  const [ modal, setModal ] = useState(false)
 
   console.log('REVIEWID V REVIEWCARD NACHALO', id)
   return (
     <>
-      {/* {isValid ? */}
         <li>
           <p>{name}</p>
           <p>{text}</p>
-        </li> :
-        {/* null} */}
-        {/* <Link to={`/adminreview`}> */}
+        </li>
         <button onClick={() => dispatch({ type: 'DELETE_FETCH_REVIEW', payload: { reviewId } })}>Удалить отзыв</button>
         <button onClick={() => dispatch({ type: 'APPROVE_FETCH_REVIEW', payload: { reviewId } })}>Пропустить отзыв</button>
-        {/* </Link> */}
+        <button onClick={() => setModal(!modal)}>Править отзыв</button>
+        { modal && <Modal name={name} text={text} id={reviewId}/> }
+
     </>
   );
 }
 
-export default ReviewCard;
+export default ReviewCardDelete;
